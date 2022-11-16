349a9997927a7cd819bc30333e85469e8ca6336c
ImKunYoung <ur2kunyoung2@outlook.com>
2022-11-17 02:03:29 +0900

    init commit

---
+++ src/test/java/com/tobias/springdatamongodb/SpringDataMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.springdatamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
